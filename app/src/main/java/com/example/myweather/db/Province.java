9fe4c16d8cedb8088eb8f12bef4618b763ac253b
yuchengren <271455361@qq.com>
2017-04-18 23:55:56 +0800

    First commit.

---
+++ app/src/main/java/com/example/myweather/db/Province.java
@@ -0,0 +1,36 @@
+package com.example.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/4/17.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+}
